58d496d2f2197875d8b18a0eb4de048b738dffd8
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-03-03 20:14:27 -0800

    seeded data

--- db.js
+++ db.js
@@ -5,10 +5,31 @@ client.connect();
 
 const sync = async() => {
     const SQL = `
+        CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
         DROP TABLE IF EXISTS recipes;
-        CREATE TABLE recipes (
+        DROP TABLE IF EXISTS chefs;
+        CREATE TABLE chefs(
+            id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
             name VARCHAR(100)
         );
+        CREATE TABLE recipes (
+            id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+            chef UUID REFERENCES chefs(id),
+            name VARCHAR(100)
+        );
+        INSERT INTO chefs(name) values('Moe');
+        INSERT INTO chefs(name) values('Larry');
+        INSERT INTO chefs(name) values('Sam');
+        INSERT INTO recipes(chef, name) values(
+            (SELECT id FROM chefs WHERE name = 'Larry'),
+            'Pot Roast');
+        INSERT INTO recipes(chef, name) values(
+            (SELECT id FROM chefs WHERE name = 'Moe'),
+            'Chicken Pot Pie');
+        INSERT INTO recipes(chef, name) values(
+            (SELECT id FROM chefs WHERE name = 'Sam'),
+            'Pizza');
+
     `;
 
     client.query(SQL);
